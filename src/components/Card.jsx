3c945323594307122474edaf350904bc488aa973
Alethepata <alessia.patane@gmail.com>
2024-06-21 16:57:17 +0200

    edit Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,43 +1,43 @@
 function Card({posts}){
-  <div className="card-container">
-    {
-      posts.map((post, index) => (
-          <div key={`card_${index}`} className="card">
+  return(
+    <div className="card-container">
+      {
+        posts.map(post =>(
+          <div key={`post_${post.id}`} className="card">
+            
+            <figure>
+              <img src={post.image} alt={post.title} />
+            </figure>
+            
+            <div className="text">
 
-              <figure>
-                  <img src={post.image} alt="" />
-              </figure>
-
-              <div className="text">
-                  
-                <div className="title">
-                  <h3>{post.title}</h3>
-                  {
-                      post.published === true ||
-                      <span>Bozza</span>
-                  }
-                </div>
-
-                <div className="content">
-                  <p>{post.content}</p>
-                </div>
-  
-                <div className="category">
-                  <span>{post.category}</span>
-                </div>
-                    
-                <div className="tags">
-                    {
-                      post.tags.map(tag => <span key={`card_tag${tag.id}`}>{tag.name}</span>)
-                    }
-                </div>
-
+              <div className="title">
+                <h3>{post.title}</h3>
+                {
+                    post.published === true ||
+                    <span>Bozza</span>
+                }
               </div>
 
+              <div className="content">
+                <p>{post.content}</p>
+              </div>
 
+              <div className="category">
+                  <span>{post.category.name}</span>
+              </div>
+
+              <div className="tags">
+                {
+                  post.tags.map(tag => <span key={`card_tag${tag.id}`}>{tag.name}</span>)
+                }
+              </div>
+
+            </div>
           </div>
-      ))
-    }
+        ))
+      }
   </div>
+  )
 }
 export default Card
